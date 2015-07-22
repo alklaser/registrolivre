9b061626cd1e3664acb722b3c8079860a536ccc6
Junior Vanin <jvanin@thoughtworks.com>
2015-07-22 18:08:19 -0300

    Eduardo|Junior - setting up gradle and springboot dependency added.

---
+++ src/main/java/br/com/registrolivre/Hello.java
@@ -0,0 +1,14 @@
+package br.com.registrolivre;
+
+public class Hello {
+
+    private final String helloMessage;
+
+    public Hello(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
+    public String getHelloMessage() {
+        return helloMessage;
+    }
+}
