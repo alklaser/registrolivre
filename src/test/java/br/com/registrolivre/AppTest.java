0b4a519677af6016431398bb2a74a2fcf435f828
Junior Vanin <jvanin@thoughtworks.com>
2015-07-22 18:25:41 -0300

    Eduardo|Junior - Mockito dependency added to the project.

--- src/test/java/br/com/registrolivre/AppTest.java
+++ src/test/java/br/com/registrolivre/AppTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AppTest {
 
+
+    Hello hello = mock(Hello.class);
+
     @Test
     public void someDummyTest() throws Exception {
-        assertThat("test", is("test"));
+        when(hello.getHelloMessage()).thenReturn("test");
+        assertThat("test", is(hello.getHelloMessage()));
     }
 }
