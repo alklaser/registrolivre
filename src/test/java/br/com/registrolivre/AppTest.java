86982812a9b70ff0a62a0d3c55f9cb4d75631ee2
Junior Vanin <jvanin@thoughtworks.com>
2015-07-22 18:15:27 -0300

    Eduardo|Junior - Junit dependency included and dummy test created.

--- src/test/java/br/com/registrolivre/AppTest.java
+++ src/test/java/br/com/registrolivre/AppTest.java
@@ -1,4 +1,14 @@
 package br.com.registrolivre;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AppTest {
+
+    @Test
+    public void someDummyTest() throws Exception {
+        assertThat("test", is("test"));
+    }
 }
