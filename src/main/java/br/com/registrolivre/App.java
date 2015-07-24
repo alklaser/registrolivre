5eb20472f5ddfd766d3c9e1f7bb908216936729c
Gustavo Goulart <ggoulart@thoughtworks.com>
2015-07-24 11:37:10 -0300

    Caroline | Gustavo - Adicionando as depencias do Flyway e exemplo

--- src/main/java/br/com/registrolivre/App.java
+++ src/main/java/br/com/registrolivre/App.java
@@ -1,12 +1,23 @@
 package br.com.registrolivre;
 
+import br.com.registrolivre.sample.flyway.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+    @Autowired
+    private PersonRepository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        new App().run();
     }
+
+    public void run () {
+        System.err.println(repository.findAll());
+    }
+
 }
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE PERSON (
+	first_name varchar(255) not null,
+	last_name varchar(255) not null
+);
+
+insert into PERSON (first_name, last_name) values ('Dave', 'Syer');
+insert into PERSON (first_name, last_name) values ('Gustavo', 'Goulart');
