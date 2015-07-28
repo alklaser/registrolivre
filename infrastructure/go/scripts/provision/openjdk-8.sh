1b3982ceaea61636395db194b7fd4101655c954b
Anne Kelly <annekellyc@gmail.com>
2015-07-28 11:38:42 -0300

    Anne|Otávio - go and app using the same openjdk-8 script and create provision task for GO

--- infrastructure/go/scripts/provision/openjdk-8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "=====> Installing openjdk-8"
-sudo apt-get update
-sudo apt-get --yes install software-properties-common python-software-properties
-sudo add-apt-repository --yes ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get --yes install openjdk-8-jdk
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-i386" >> /etc/environment
--- infrastructure/scripts/provision/open-jdk8.sh
+++ infrastructure/scripts/provision/open-jdk8.sh
@@ -1,5 +1,7 @@
 echo "=====> Installing openjdk-8"
+sudo apt-get update
 sudo apt-get --yes install software-properties-common
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository --yes ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get --yes install openjdk-8-jdk
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-i386" >> /etc/environment
