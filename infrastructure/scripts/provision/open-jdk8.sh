dce46ee70229535c66e7b5226a424a069f52cefd
Junior Vanin <jvanin@thoughtworks.com>
2015-07-28 16:32:22 -0300

    Ivan|Junior - python-software-properties included for downloading openjdk 8.

--- infrastructure/scripts/provision/open-jdk8.sh
+++ infrastructure/scripts/provision/open-jdk8.sh
@@ -1,7 +1,8 @@
 echo "=====> Installing openjdk-8"
 sudo apt-get update
-sudo apt-get --yes install software-properties-common
+sudo apt-get --yes install software-properties-common  python-software-properties
 sudo add-apt-repository --yes ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get --yes install openjdk-8-jdk
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-i386" >> /etc/environment
+
