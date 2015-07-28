00003a5a365be40105e46f842c5b9a611accebb9
Anne Kelly <annekellyc@gmail.com>
2015-07-28 09:59:16 -0300

    Anne|Otávio - provisioning machine with openjdk-8

--- infrastructure/scripts/provision/open-jdk8.sh
+++ infrastructure/scripts/provision/open-jdk8.sh
@@ -1,6 +1,5 @@
 echo "=====> Installing openjdk-8"
-sudo apt-get --yes install software-properties-common python-software-properties
+sudo apt-get --yes install software-properties-common
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get --yes install openjdk-8-jdk
-sudo update-alternatives --config java
