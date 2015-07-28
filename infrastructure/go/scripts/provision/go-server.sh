77cb46a58153c5c2bc29c61c94d649488409b911
Junior Vanin <jvanin@thoughtworks.com>
2015-07-28 15:03:59 -0300

    Ivan|Junior - Arquivo xml com a configuração do GO-CD criado.

--- infrastructure/go/scripts/provision/go-server.sh
+++ infrastructure/go/scripts/provision/go-server.sh
@@ -4,3 +4,7 @@ sudo echo "deb http://dl.bintray.com/gocd/gocd-deb/ /" > /etc/apt/sources.list.d
 wget --quiet -O - "https://bintray.com/user/downloadSubjectPublicKey?username=gocd" | sudo apt-key add -
 sudo apt-get update
 sudo apt-get --yes install go-server
+
+/etc/init.d/go-server stop
+cp /vagrant/configurations/cruise-config.xml /etc/go
+/etc/init.d/go-server start
