06b4a1396dad8fdd7ba4039307aeb30695c9b111
OtavioRMachado <omachado@thoughtworks.com>
2015-07-31 15:20:12 -0300

    Otavio | Gustavo - Adding modified create-user shellscript.

--- infrastructure/application/scripts/provision/create-user.sh
+++ infrastructure/application/scripts/provision/create-user.sh
@@ -6,5 +6,6 @@ USER="registrolivre"
 echo "=====> Creating registrolivre user"
 if [ -z "$(getent passwd ${USER})" ]; then
     sudo useradd -d /home/${USER} -s /bin/bash -m ${USER}
+    sudo usermod -a -G sudo ${USER}
+    sudo usermod -a -G admin ${USER}
 fi
-
