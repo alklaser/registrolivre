3ef2a173c7c7b11f1de34111c03b6abbc3f896c0
Junior Vanin <jvanin@thoughtworks.com>
2015-07-31 16:42:01 -0300

    Eduardo|Junior - fixing provision script.

--- infrastructure/go/scripts/deploy/production/provision-to-aws.sh
+++ infrastructure/go/scripts/deploy/production/provision-to-aws.sh
@@ -8,6 +8,7 @@ sh copy-files.sh
 
 echo "=====> Running provision scripts"
 ssh -i $AWS_GO_KEY $AWS_USER@$AWS_GO_IP_OR_DNS "sh /home/$AWS_USER/provision/open-jdk8.sh"
+ssh -i $AWS_GO_KEY $AWS_USER@$AWS_GO_IP_OR_DNS "sh /home/$AWS_USER/provision/export_java_home.sh"
 ssh -i $AWS_GO_KEY $AWS_USER@$AWS_GO_IP_OR_DNS "sh /home/$AWS_USER/provision/git.sh"
 ssh -i $AWS_GO_KEY $AWS_USER@$AWS_GO_IP_OR_DNS "sh /home/$AWS_USER/provision/go-server.sh"
 ssh -i $AWS_GO_KEY $AWS_USER@$AWS_GO_IP_OR_DNS "sh /home/$AWS_USER/provision/go-agent.sh"
