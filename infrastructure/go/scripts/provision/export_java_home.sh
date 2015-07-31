8963c65fef7a3037483945d1e92e0c5ad5a8e301
Junior Vanin <jvanin@thoughtworks.com>
2015-07-31 13:40:23 -0300

    Eduardo|Junior - export_java_home changed to used sudo access.

--- infrastructure/go/scripts/provision/export_java_home.sh
+++ infrastructure/go/scripts/provision/export_java_home.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-echo "=====> Exporting JAVA_HOME"
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >> /etc/environment
+sudo su <<BLOCK
+    echo "=====> Exporting JAVA_HOME"
+    echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >> /etc/environment
+BLOCK
