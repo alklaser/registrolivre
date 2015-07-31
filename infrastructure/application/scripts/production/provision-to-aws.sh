bae0d498fcad851dab70fbb425321c7798873b41
Anne Kelly <annekellyc@gmail.com>
2015-07-31 16:55:06 -0300

    Anne - fixing scripts

--- infrastructure/application/scripts/production/provision-to-aws.sh
+++ infrastructure/application/scripts/production/provision-to-aws.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
 set -e
-SCRIPTS_PATH="/home/ubuntu/scripts"
-PROVISION_PATH="/home/ubuntu/scripts/provision"
+SCRIPTS="/home/ubuntu/scripts"
+PROVISION="/home/ubuntu/scripts/provision"
+PUBLIC_KEYS="/home/public_keys"
 USER="/home/registrolivre"
 
 echo "=====> Creating scripts folder"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${SCRIPTS_PATH}"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir ${SCRIPTS_PATH}"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 ${SCRIPTS_PATH}"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir ${SCRIPTS}"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 ${SCRIPTS}"
 
 echo "=====> Copying script files"
-scp -i $AWS_APP_KEY -r ./scripts/provision/ ubuntu@$AWS_IP_OR_DNS:${SCRIPTS_PATH}
-scp -i $AWS_APP_KEY -r ../scripts/provision/open-jdk8.sh ubuntu@$AWS_IP_OR_DNS:${PROVISION_PATH}
+scp -i $AWS_PRIVATE_KEY -r ./scripts/provision/ ubuntu@$AWS_IP_OR_DNS:${SCRIPTS}
+scp -i $AWS_PRIVATE_KEY -r ../scripts/provision/open-jdk8.sh ubuntu@$AWS_IP_OR_DNS:${PROVISION}
 
 echo "=====> Creating public keys folder"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf /home/public_keys"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir -p /home/public_keys"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 /home/public_keys"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir -p ${PUBLIC_KEYS}"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 ${PUBLIC_KEYS}"
 
 echo "=====> Copying public keys folder"
-scp -i $AWS_APP_KEY -r ./public_keys/* ubuntu@$AWS_IP_OR_DNS:/home/public_keys
+scp -i $AWS_PRIVATE_KEY -r ./public_keys/* ubuntu@$AWS_IP_OR_DNS:/home/public_keys
 
 echo "=====> Provisioning ..."
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/open-jdk8.sh"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/create-user.sh"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/ssh-user-permission.sh"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/create-folders.sh"
-scp -i $AWS_APP_KEY ./scripts/provision/application-service.sh ubuntu@$AWS_IP_OR_DNS:${USER}/service
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/remove-application-service.sh"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION_PATH}/create-application-service.sh"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/open-jdk8.sh"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-user.sh"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/ssh-user-permission.sh"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-folders.sh"
+scp -i $AWS_PRIVATE_KEY ./scripts/provision/application-service.sh ubuntu@$AWS_IP_OR_DNS:${USER}/service
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/remove-application-service.sh"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-application-service.sh"
 echo "=====> End Provision ..."
 
 
 echo "=====> Removing provision files"
-ssh -i $AWS_APP_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${SCRIPTS_PATH}"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${SCRIPTS}"
+ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${PUBLIC_KEYS}"
--- infrastructure/database/scripts/create-database.sh
+++ infrastructure/database/scripts/create-database.sh
@@ -7,6 +7,6 @@ DATABASE="/home/registrolivre/database"
 echo "=====> Creating database"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo mkdir -p ${DATABASE}"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo chmod 777 ${DATABASE}"
-scp -i $REGISTROLIVRE_PRIVATE_KEY /Users/omachado/projects/registrolivre/infrastructure/database/scripts/postgres-install.sh registrolivre@$REGISTROLIVRE_IP_OR_DNS:${DATABASE}
+scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/postgres-install.sh registrolivre@$REGISTROLIVRE_IP_OR_DNS:${DATABASE}
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "bash -s < ${DATABASE}/postgres-install.sh"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo rm -rf ${DATABASE}"
