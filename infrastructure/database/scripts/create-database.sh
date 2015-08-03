c9091edd50616d15fc5822be47973a4dcbba946c
Anne Kelly <annekellyc@gmail.com>
2015-08-03 13:31:20 -0300

    Anne|Caroline - Fix environment variable name.

--- infrastructure/database/scripts/create-database.sh
+++ infrastructure/database/scripts/create-database.sh
@@ -5,9 +5,9 @@ set -e
 DATABASE="/home/registrolivre/database"
 
 echo "=====> Creating database"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo mkdir -p ${DATABASE}"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo chmod 777 ${DATABASE}"
-scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/postgres-install.sh registrolivre@$REGISTROLIVRE_IP_OR_DNS:${DATABASE}
-scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/apt.postgresql.org.sh registrolivre@$REGISTROLIVRE_IP_OR_DNS:${DATABASE}
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "bash -s < ${DATABASE}/postgres-install.sh"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo rm -rf ${DATABASE}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo mkdir -p ${DATABASE}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo chmod 777 ${DATABASE}"
+scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/postgres-install.sh registrolivre@$LOCAL_REGISTROLIVRE_IP:${DATABASE}
+scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/apt.postgresql.org.sh registrolivre@$LOCAL_REGISTROLIVRE_IP:${DATABASE}
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "bash -s < ${DATABASE}/postgres-install.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo rm -rf ${DATABASE}"
--- infrastructure/database/scripts/destroy-database.sh
+++ infrastructure/database/scripts/destroy-database.sh
@@ -5,8 +5,8 @@ set -e
 DATABASE="/home/registrolivre/database"
 
 echo "=====> Destroying database"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo mkdir -p ${DATABASE}"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo chmod 777 ${DATABASE}"
-scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/postgres-uninstall.sh registrolivre@$REGISTROLIVRE_IP_OR_DNS:${DATABASE}
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "bash -s < ${DATABASE}/postgres-uninstall.sh"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$REGISTROLIVRE_IP_OR_DNS "sudo rm -rf ${DATABASE}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo mkdir -p ${DATABASE}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo chmod 777 ${DATABASE}"
+scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/postgres-uninstall.sh registrolivre@$LOCAL_REGISTROLIVRE_IP:${DATABASE}
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "bash -s < ${DATABASE}/postgres-uninstall.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "sudo rm -rf ${DATABASE}"
