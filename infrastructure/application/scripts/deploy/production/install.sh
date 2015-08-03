5da50517e056f82e779f4bcb4572d607ebe2a05a
Anne Kelly <annekellyc@gmail.com>
2015-08-03 15:24:37 -0300

    Anne|Caroline - fixing jar path.

--- infrastructure/application/scripts/deploy/production/install.sh
+++ infrastructure/application/scripts/deploy/production/install.sh
@@ -3,9 +3,10 @@
 set -e
 
 APP="/home/registrolivre/app"
+ROOT_PATH="./../../"
 
 echo "=====> Generate jar"
-./../../gradlew :registroLivreJar
+${ROOT_PATH}gradlew :registroLivreJar
 
 echo "=====> Stopping registrolivre service"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$AWS_REGISTROLIVRE_IP "service registrolivre stop"
@@ -14,7 +15,7 @@ echo "=====> Removing registrolivre jar"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$AWS_REGISTROLIVRE_IP "rm -rf ${APP}/registrolivre.jar"
 
 echo "=====> Copy jar to app folder"
-scp -i $REGISTROLIVRE_PRIVATE_KEY ~/Documents/registrolivre.jar registrolivre@$AWS_REGISTROLIVRE_IP:${APP}/
+scp -i $REGISTROLIVRE_PRIVATE_KEY ${ROOT_PATH}registrolivre.jar registrolivre@$AWS_REGISTROLIVRE_IP:${APP}/
 
 echo "=====> Starting registrolivre service"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$AWS_REGISTROLIVRE_IP "service registrolivre start"
