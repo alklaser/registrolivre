afc1a6c14af440a1d27b4016a9f55fe35088b87a
Anne Kelly <annekellyc@gmail.com>
2015-08-03 11:41:53 -0300

    Anne|Caroline - Script to: provision to AWS, deploy application to local, deply application to aws. Rename environment variables.

--- infrastructure/application/scripts/deploy/local/install.sh
+++ infrastructure/application/scripts/deploy/local/install.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
 set -e
+
+APP="/home/registrolivre/app"
+
+echo "=====> Generate jar"
+./../../gradlew :registroLivreJar
+
+echo "=====> Stopping registrolivre service"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "service registrolivre stop"
+
+echo "=====> Removing registrolivre jar"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "rm -rf ${APP}/registrolivre.jar"
+
+echo "=====> Copy jar to app folder"
+scp -i $REGISTROLIVRE_PRIVATE_KEY ~/Documents/registrolivre.jar registrolivre@$LOCAL_REGISTROLIVRE_IP:${APP}/
+
+echo "=====> Starting registrolivre service"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "service registrolivre start"
--- infrastructure/application/scripts/production/provision-to-aws.sh
+++ infrastructure/application/scripts/production/provision-to-aws.sh
@@ -7,31 +7,31 @@ PUBLIC_KEYS="/home/public_keys"
 USER="/home/registrolivre"
 
 echo "=====> Creating scripts folder"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir ${SCRIPTS}"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 ${SCRIPTS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo mkdir ${SCRIPTS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo chmod 777 ${SCRIPTS}"
 
 echo "=====> Copying script files"
-scp -i $AWS_PRIVATE_KEY -r ./scripts/provision/ ubuntu@$AWS_IP_OR_DNS:${SCRIPTS}
-scp -i $AWS_PRIVATE_KEY -r ../scripts/provision/open-jdk8.sh ubuntu@$AWS_IP_OR_DNS:${PROVISION}
+scp -i $REGISTROLIVRE_PRIVATE_KEY -r ./scripts/provision/ ubuntu@$AWS_REGISTROLIVRE_IP:${SCRIPTS}
+scp -i $REGISTROLIVRE_PRIVATE_KEY -r ../scripts/provision/open-jdk8.sh ubuntu@$AWS_REGISTROLIVRE_IP:${PROVISION}
 
 echo "=====> Creating public keys folder"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo mkdir -p ${PUBLIC_KEYS}"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo chmod 777 ${PUBLIC_KEYS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo mkdir -p ${PUBLIC_KEYS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo chmod 777 ${PUBLIC_KEYS}"
 
 echo "=====> Copying public keys folder"
-scp -i $AWS_PRIVATE_KEY -r ./public_keys/* ubuntu@$AWS_IP_OR_DNS:/home/public_keys
+scp -i $REGISTROLIVRE_PRIVATE_KEY -r ./public_keys/* ubuntu@$AWS_REGISTROLIVRE_IP:/home/public_keys
 
 echo "=====> Provisioning ..."
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/open-jdk8.sh"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-user.sh"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/ssh-user-permission.sh"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-folders.sh"
-scp -i $AWS_PRIVATE_KEY ./scripts/provision/application-service.sh ubuntu@$AWS_IP_OR_DNS:${USER}/service
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/remove-application-service.sh"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo sh ${PROVISION}/create-application-service.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/open-jdk8.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/create-user.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/ssh-user-permission.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/create-folders.sh"
+scp -i $REGISTROLIVRE_PRIVATE_KEY ./scripts/provision/application-service.sh ubuntu@$AWS_REGISTROLIVRE_IP:${USER}/service
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/remove-application-service.sh"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo sh ${PROVISION}/create-application-service.sh"
 echo "=====> End Provision ..."
 
 
 echo "=====> Removing provision files"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${SCRIPTS}"
-ssh -i $AWS_PRIVATE_KEY ubuntu@$AWS_IP_OR_DNS "sudo rm -rf ${PUBLIC_KEYS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo rm -rf ${SCRIPTS}"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY ubuntu@$AWS_REGISTROLIVRE_IP "sudo rm -rf ${PUBLIC_KEYS}"
