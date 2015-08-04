f4f6d7f85a70bbdef6e9247aec4d07fcbbef62ff
Junior Vanin <jvanin@thoughtworks.com>
2015-08-04 10:45:47 -0300

    Eduardo|Junior - scripts for deploy updated with new enviroment variables and path of vagrant up fixed in build.gradle file.

--- infrastructure/go/scripts/production/deploy-go-agent-on-aws.sh
+++ infrastructure/go/scripts/production/deploy-go-agent-on-aws.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Use this command to access GO instance in aws
-#ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS
+#ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP
 
 echo "=====> Starting configuration Go Agent on AWS machine"
 SCRIPTS_PATH="/tmp/go"
@@ -9,27 +9,27 @@ PROVISION_PATH="./scripts/provision/."
 CONFIGURATIONS_PATH="./configurations/."
 
 echo "=====> Creating scripts folder"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sudo rm -rf ${SCRIPTS_PATH}"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sudo mkdir ${SCRIPTS_PATH}"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sudo chmod 777 ${SCRIPTS_PATH}"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sudo rm -rf ${SCRIPTS_PATH}"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sudo mkdir ${SCRIPTS_PATH}"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sudo chmod 777 ${SCRIPTS_PATH}"
 
 
 echo "=====> Copying script files"
-scp -i $AWS_GO_AGENT_KEY -r ${PROVISION_PATH} ubuntu@$AWS_GO_AGENT_DNS:${SCRIPTS_PATH}
-scp -i $AWS_GO_AGENT_KEY -r ${CONFIGURATIONS_PATH} ubuntu@$AWS_GO_AGENT_DNS:${SCRIPTS_PATH}
-scp -i $AWS_GO_AGENT_KEY -r ./../scripts/provision/open-jdk8.sh ubuntu@$AWS_GO_AGENT_DNS:${SCRIPTS_PATH}
+scp -i $GO_AGENT_REGISTROLIVRE_KEY -r ${PROVISION_PATH} ubuntu@$GO_AGENT_AWS_IP:${SCRIPTS_PATH}
+scp -i $GO_AGENT_REGISTROLIVRE_KEY -r ${CONFIGURATIONS_PATH} ubuntu@$GO_AGENT_AWS_IP:${SCRIPTS_PATH}
+scp -i $GO_AGENT_REGISTROLIVRE_KEY -r ./../scripts/provision/open-jdk8.sh ubuntu@$GO_AGENT_AWS_IP:${SCRIPTS_PATH}
 
 echo "=====> Running provision scripts"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/open-jdk8.sh"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/export-java_home.sh"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/git.sh"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/nodejs.sh"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/go-agent.sh"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sh ${SCRIPTS_PATH}/export-go_server.sh $AWS_GO_SERVER_DNS"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/open-jdk8.sh"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/export-java_home.sh"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/git.sh"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/nodejs.sh"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/go-agent.sh"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sh ${SCRIPTS_PATH}/export-go_server.sh $AWS_GO_SERVER_DNS"
 
 
 echo "=====> Removing provision files"
-ssh -i $AWS_GO_AGENT_KEY ubuntu@$AWS_GO_AGENT_DNS "sudo rm -rf ${SCRIPTS_PATH}"
+ssh -i $GO_AGENT_REGISTROLIVRE_KEY ubuntu@$GO_AGENT_AWS_IP "sudo rm -rf ${SCRIPTS_PATH}"
 
 
 
--- infrastructure/go/scripts/production/deploy-go-server-on-aws.sh
+++ infrastructure/go/scripts/production/deploy-go-server-on-aws.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Use this command to access GO instance in aws
-#ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS
+#ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP
 
 echo "=====> Starting configuration Go Server on AWS machine"
 PROVISION_PATH="./scripts/provision/."
@@ -9,24 +9,24 @@ CONFIGURATIONS_PATH="./configurations/."
 SCRIPTS_PATH="/tmp/go"
 
 echo "=====> Creating scripts folder"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sudo rm -rf ${SCRIPTS_PATH}"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sudo mkdir ${SCRIPTS_PATH}"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sudo chmod 777 ${SCRIPTS_PATH}"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sudo rm -rf ${SCRIPTS_PATH}"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sudo mkdir ${SCRIPTS_PATH}"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sudo chmod 777 ${SCRIPTS_PATH}"
 
 
 echo "=====> Copying script files"
-scp -i $AWS_GO_SERVER_KEY -r ${PROVISION_PATH} ubuntu@$AWS_GO_SERVER_DNS:${SCRIPTS_PATH}
-scp -i $AWS_GO_SERVER_KEY -r ${CONFIGURATIONS_PATH} ubuntu@$AWS_GO_SERVER_DNS:${SCRIPTS_PATH}
-scp -i $AWS_GO_SERVER_KEY -r ./../scripts/provision/open-jdk8.sh ubuntu@$AWS_GO_SERVER_DNS:${SCRIPTS_PATH}
+scp -i $GO_SERVER_REGISTROLIVRE_KEY -r ${PROVISION_PATH} ubuntu@$GO_SERVER_AWS_IP:${SCRIPTS_PATH}
+scp -i $GO_SERVER_REGISTROLIVRE_KEY -r ${CONFIGURATIONS_PATH} ubuntu@$GO_SERVER_AWS_IP:${SCRIPTS_PATH}
+scp -i $GO_SERVER_REGISTROLIVRE_KEY -r ./../scripts/provision/open-jdk8.sh ubuntu@$GO_SERVER_AWS_IP:${SCRIPTS_PATH}
 
 echo "=====> Running provision scripts"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sh ${SCRIPTS_PATH}/open-jdk8.sh"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sh ${SCRIPTS_PATH}/export-java_home.sh"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sh ${SCRIPTS_PATH}/go-server.sh"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sh ${SCRIPTS_PATH}/open-jdk8.sh"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sh ${SCRIPTS_PATH}/export-java_home.sh"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sh ${SCRIPTS_PATH}/go-server.sh"
 
 
 echo "=====> Removing provision files"
-ssh -i $AWS_GO_SERVER_KEY ubuntu@$AWS_GO_SERVER_DNS "sudo rm -rf ${SCRIPTS_PATH}"
+ssh -i $GO_SERVER_REGISTROLIVRE_KEY ubuntu@$GO_SERVER_AWS_IP "sudo rm -rf ${SCRIPTS_PATH}"
 
 
 
