35b8cde1e977f1c2474380bbf7c3003be77c8f80
Julio Farah <julio.farah@gmail.com>
2015-08-11 15:23:06 -0300

    @juliofarah @juniorvanin - wip #1 - fix routes

--- src/main/java/br/com/registrolivre/controllers/HomeController.java
+++ src/main/java/br/com/registrolivre/controllers/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String movies() {
+    @RequestMapping("/home")
+    public String home() {
         return "index";
     }
 }
