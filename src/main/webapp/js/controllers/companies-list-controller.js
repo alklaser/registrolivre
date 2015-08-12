6c0e5610b9bde1efe30bd21da02de4078d2c20a4
Anne Kelly <annekellyc@gmail.com>
2015-08-12 13:45:52 -0300

    @annekellyc | @carolmoers - wip #2 - removing unnecessary unit test

--- src/main/webapp/js/controllers/companies-list-controller.js
+++ src/main/webapp/js/controllers/companies-list-controller.js
@@ -1,9 +1,9 @@
 app.controller("CompaniesListController", ["$scope", "companies", function($scope, companies) {
     $scope.companies = [];
 
-    $scope.companies = [{id: 1, cnpj: "123456", tradeName: "Company One Ltda"}, {id: 2, cnpj: "654321", tradeName: "Company Two, Inc"}];
+    //$scope.companies = [{id: 1, cnpj: "123456", tradeName: "Company One Ltda"}, {id: 2, cnpj: "654321", tradeName: "Company Two, Inc"}];
 
-    //companies.all().then(function(response){
-    //    $scope.companies = response;
-    //});
+    companies.all().then(function(response){
+        $scope.companies = response;
+    });
 }]);
--- src/test/webapp/js/app-spec.js
+++ src/test/webapp/js/app-spec.js
@@ -6,13 +6,4 @@ describe("App routes test", function() {
             $route.routes['/add/document'].controller.should.equal('../js/controllers/document-upload-controller');
         });
     });
-
-    it('should redirect to /add/document when entering any undocumented route', function() {
-        module('registro-livre');
-        inject(function($route) {
-            $route.routes[null].redirectTo.should.equal('/add/document');
-        });
-    });
-
-
 });
