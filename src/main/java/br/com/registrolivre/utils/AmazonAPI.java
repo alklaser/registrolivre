019c889913a465bf26f6fcf6d7fbf57abe1484af
dieselpardal <idiesel@thoughtworks.com>
2015-08-13 16:32:24 -0300

    @idiesel | @gliberat - Add example S3 keys to tests.

--- src/main/java/br/com/registrolivre/utils/AmazonAPI.java
+++ src/main/java/br/com/registrolivre/utils/AmazonAPI.java
@@ -10,9 +10,11 @@ import com.amazonaws.services.s3.AmazonS3Client;
  */
 public class AmazonAPI {
     public static final String BUCKET_NAME = "documentos-empresas";
+    public static final String ACCESS_KEY_ID = "AKIAISYASUHBH3KPVJ7A";
+    public static final String SECRET_ACCESS_KEY = "sUSTVZZUFpjaJKd5a1u53KYsP9YaqCkn7aDcfb3v";
 
     public static AmazonS3 getS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials("YourAccessKeyID", "YourSecretAccessKey");
+        AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
         return new AmazonS3Client(credentials);
     }
 }
