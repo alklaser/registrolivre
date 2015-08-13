90a2ddcc7aed5ffd442be156d586e4803e513273
Julio Farah <julio.farah@gmail.com>
2015-08-13 11:02:39 -0300

    wip #1 - change CompanyController to use RestController annotation

--- src/main/java/br/com/registrolivre/controllers/CompanyController.java
+++ src/main/java/br/com/registrolivre/controllers/CompanyController.java
@@ -8,10 +8,10 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -21,7 +21,7 @@ import java.util.Set;
 
 @Log4j
 @NoArgsConstructor
-@Controller
+@RestController
 public class CompanyController {
 
     private CompanyRepository companyRepository;
