5d48c229e83a1c536de7c52fd24d8bc8eed606db
Gustavo Goulart <ggoulart@thoughtworks.com>
2015-08-14 14:59:56 -0300

    @ebragaparah | @ggoulart | @pedrro - Upload file to amazon

--- src/main/java/br/com/registrolivre/utils/AmazonService.java
+++ src/main/java/br/com/registrolivre/utils/AmazonService.java
@@ -4,11 +4,11 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
-/**
- * Created by idiesel on 8/12/15.
- */
-public class AmazonAPI {
+import java.io.File;
+
+public class AmazonService {
     public static final String BUCKET_NAME = "registrolivre";
     public static final String ACCESS_KEY_ID = "AKIAISYASUHBH3KPVJ7A";
     public static final String SECRET_ACCESS_KEY = "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";
@@ -16,5 +16,10 @@ public class AmazonAPI {
     public static AmazonS3 getS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
         return new AmazonS3Client(credentials);
+    }
+
+    public String uploadToAmazonS3(AmazonS3 amazonS3Client, String fileName, File file) {
+        amazonS3Client.putObject(BUCKET_NAME, fileName, file);
+        return amazonS3Client.generatePresignedUrl(BUCKET_NAME, fileName, null).toString();
     }
 }
