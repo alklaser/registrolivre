5975d70cbc5e1b795f2cae61962aa8ccc8cb0def
Julio Farah <julio.farah@gmail.com>
2015-08-15 23:43:05 -0300

    wip #1 - encapsulate cnpjValidator stuff

--- src/main/resources/static/utils/cnpjValidator.js
+++ src/main/resources/static/utils/cnpjValidator.js
@@ -1,55 +1,62 @@
-function areAllCharsTheSame(input) {
-  for (i = 0; i < input.length - 1; i++) {
-    if (input.charAt(i) != input.charAt(i + 1)) {
+cnpjValidator = function(input) {
+  var validateCNPJ = function(input) {
+    alert('bzaahh')
+
+    cnpj = input.replace(/[^\d]+/g, '');
+
+    return isCNPJStructureValid(cnpj) &&
+      firstDigitValidation(cnpj) &&
+      secondDigitValidation(cnpj);
+  };
+
+  var isCNPJStructureValid = function(cnpj) {
+    if (cnpj == '')
       return false;
+
+    if (cnpj.length != 14)
+      return false;
+
+    if (areAllCharsTheSame(cnpj))
+      return false;
+
+    return true;
+  };
+
+  var areAllCharsTheSame = function(input) {
+    for (i = 0; i < input.length - 1; i++) {
+      if (input.charAt(i) != input.charAt(i + 1)) {
+        return false;
+      }
     }
-  }
 
-  return true;
-}
+    return true;
+  };
 
-function validateCNPJ(input) {
+  var firstDigitValidation = function(cnpj) {
+    size = cnpj.length - 2;
+    digits = cnpj.substring(size);
+    return cnpjDigitCalculation(cnpj, size) == digits.charAt(0);
+  };
 
-  cnpj = input.replace(/[^\d]+/g, '');
+  var secondDigitValidation = function(cnpj) {
+    size = cnpj.length - 1;
+    digits = cnpj.substring(size);
+    return cnpjDigitCalculation(cnpj, size) == digits.charAt(0);
+  };
 
-  return isCNPJStructureValid(cnpj) &&
-         firstDigitValidation(cnpj) &&
-         secondDigitValidation(cnpj);
-}
+  var cnpjDigitCalculation = function(cnpj, size) {
+    numbers = cnpj.substring(0, size);
+    sum = 0;
+    pos = size - 7;
+    for (i = size; i >= 1; i--) {
+      sum += numbers.charAt(size - i) * pos--;
+      if (pos < 2)
+        pos = 9;
+    }
+    return sum % 11 < 2 ? 0 : 11 - sum % 11;
+  };
 
-var firstDigitValidation = function(cnpj) {
-  size = cnpj.length - 2;
-  digits = cnpj.substring(size);
-  return cnpjDigitCalculation(cnpj, size) == digits.charAt(0);
-}
-
-var secondDigitValidation = function(cnpj) {
-  size = cnpj.length - 1;
-  digits = cnpj.substring(size);
-  return CNPJDigitCalculation(cnpj, size) == digits.charAt(0);
-}
-
-var cnpjDigitCalculation = function(cnpj, size) {
-  numbers = cnpj.substring(0, size);
-  sum = 0;
-  pos = size - 7;
-  for (i = size; i >= 1; i--) {
-    sum += numbers.charAt(size - i) * pos--;
-    if (pos < 2)
-      pos = 9;
-  }
-  return sum % 11 < 2 ? 0 : 11 - sum % 11;
-}
-
-var isCNPJStructureValid = function(cnpj) {
-  if (cnpj == '')
-    return false;
-
-  if (cnpj.length != 14)
-    return false;
-
-  if (areAllCharsTheSame(cnpj))
-    return false;
-
-  return true;
+  return  {
+    validateCNPJ : validateCNPJ
+  };
 };
