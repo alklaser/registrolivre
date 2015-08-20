fd9e989bbf6b03652486c43101932519e2a44cd5
Anne Kelly <annekellyc@gmail.com>
2015-08-20 11:20:22 -0300

    @annekellyc | @ebragaparah - wip #3 - refactoring aws service

---
+++ src/main/java/br/com/registrolivre/services/AWSService.java
@@ -0,0 +1,25 @@
+package br.com.registrolivre.services;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+
+import static br.com.registrolivre.utils.EnvironmentVariables.REGISTROLIVRE_AWS_ACCESS_KEY_ID;
+import static br.com.registrolivre.utils.EnvironmentVariables.REGISTROLIVRE_AWS_SECRET_ACCESS_KEY;
+
+public class AWSService {
+    public static final String BUCKET_NAME = "registrolivre";
+
+    public static AmazonS3 client() {
+        AWSCredentials credentials = new BasicAWSCredentials(REGISTROLIVRE_AWS_ACCESS_KEY_ID, REGISTROLIVRE_AWS_SECRET_ACCESS_KEY);
+        return new AmazonS3Client(credentials);
+    }
+
+    public String uploadToS3(AmazonS3 client, String fileName, File file) {
+        client.putObject(BUCKET_NAME, fileName, file);
+        return client.generatePresignedUrl(BUCKET_NAME, fileName, null).toString();
+    }
+}
