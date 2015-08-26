56f7cd17563364b02b08ce021f298ce28317d1db
Gustavo Goulart <ggoulart@thoughtworks.com>
2015-08-26 15:52:10 -0300

    @ggoulart - Refactor CompaniesController

--- src/main/java/br/com/registrolivre/controllers/CompaniesController.java
+++ src/main/java/br/com/registrolivre/controllers/CompaniesController.java
@@ -3,21 +3,21 @@ package br.com.registrolivre.controllers;
 import br.com.registrolivre.controllers.representations.CompanyRepresentation;
 import br.com.registrolivre.models.Company;
 import br.com.registrolivre.services.CompanyService;
-import com.google.common.collect.Lists;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static com.google.common.collect.Lists.*;
+import static java.util.stream.Collectors.*;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Log4j
 @NoArgsConstructor
@@ -31,23 +31,23 @@ public class CompaniesController {
         this.companyService = companyService;
     }
 
-    @RequestMapping(value = "/empresas", method = RequestMethod.GET)
+    @RequestMapping(value = "/empresas", method = GET)
     public
     @ResponseBody
     ResponseEntity<Iterable<CompanyRepresentation>> getCompanies() {
-
         try {
-            Iterable<Company> registeredCompanies = companyService.findAll();
-
-            List<CompanyRepresentation> companies = Lists.newArrayList(registeredCompanies)
-                    .stream()
-                    .map(company -> toRepresentation(company))
-                    .collect(Collectors.toList());
-            return ok(companies);
+            return ok(companies());
         } catch (IllegalArgumentException ex) {
             log.error("Error trying to find one or more companies", ex);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private List<CompanyRepresentation> companies() {
+        return newArrayList(companyService.findAll())
+                .stream()
+                .map(this::toRepresentation)
+                .collect(toList());
     }
 
     private CompanyRepresentation toRepresentation(Company company) {
