5a87e9055b6251bcb57b3af10eb50b253733e8ed
Anne Kelly <annekellyc@gmail.com>
2015-08-26 15:20:57 -0300

    @annekellyc | Mayra - refactoring variable names and method name.

--- src/main/java/br/com/registrolivre/configurations/DataSourceConfiguration.java
+++ src/main/java/br/com/registrolivre/configurations/DataSourceConfiguration.java
@@ -13,15 +13,15 @@ public class DataSourceConfiguration {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv(DatabaseEnvironmentVariables.URL));
+        URI databaseUri = new URI(System.getenv(DatabaseEnvironmentVariables.URL));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String databaseUrl = String.format("jdbc:postgresql://%s:%s%s", dbUri.getHost(), dbUri.getPort(), dbUri.getPath());
+        String userName = databaseUri.getUserInfo().split(":")[0];
+        String password = databaseUri.getUserInfo().split(":")[1];
+        String databaseUrl = String.format("jdbc:postgresql://%s:%s%s", databaseUri.getHost(), databaseUri.getPort(), databaseUri.getPath());
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(databaseUrl);
-        basicDataSource.setUsername(username);
+        basicDataSource.setUsername(userName);
         basicDataSource.setPassword(password);
 
         return basicDataSource;
