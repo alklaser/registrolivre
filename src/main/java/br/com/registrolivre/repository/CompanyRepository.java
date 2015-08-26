ba2b3aba3c2f26c7fb59a7f199dc523b4603deeb
Giovane <giovaneliberato@gmail.com>
2015-08-26 15:09:59 -0300

    Remove unused import

--- src/main/java/br/com/registrolivre/repository/CompanyRepository.java
+++ src/main/java/br/com/registrolivre/repository/CompanyRepository.java
@@ -4,8 +4,6 @@ import br.com.registrolivre.models.Company;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, Long> {
     Company getByCnpj(String cnpj);
